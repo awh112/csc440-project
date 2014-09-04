bbdfb07d939e3807f35eaee5ef81aaab7d3946f9
Adam Howard <awhoward112@gmail.com>
2014-09-03 21:14:27 -0400

    initial commit

---
+++ CSC440-Project/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSC440_Project.Startup))]
+namespace CSC440_Project
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

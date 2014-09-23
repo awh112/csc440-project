e05398cf5807771459f6205049f782c00f0e8bb8
Adam Howard <awhoward112@gmail.com>
2014-09-22 21:37:01 -0400

    made changes to fix the migrations - moved code over to create table

--- CSC440-Project/Migrations/201409171740083_ChangeOccupationalCode.cs
+++ CSC440-Project/Migrations/201409171740083_ChangeOccupationalCode.cs
@@ -7,6 +7,41 @@ namespace CSC440_Project.Migrations
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.DetailedOccupations",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    OccupationalCode = c.Int(nullable: false),
+                    Title = c.String(),
+                    OccupationalGroupId = c.Int(nullable: false),
+                    CurrentEmployment = c.Int(nullable: false),
+                    FutureEmployment = c.Int(nullable: false),
+                    NumberChange = c.Int(nullable: false),
+                    PercentageChange = c.Int(nullable: false),
+                    OpeningsAndReplacmentsGrowth = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.OccupationalGroups", t => t.OccupationalGroupId, cascadeDelete: true)
+                .Index(t => t.OccupationalGroupId);
+
+            CreateTable(
+                "dbo.OccupationalGroups",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    OccupationalCode = c.Int(nullable: false),
+                    GroupName = c.String(),
+                    CurrentEmploymentNumber = c.Int(nullable: false),
+                    FutureEmploymentNumber = c.Int(nullable: false),
+                    PercentChange = c.Double(nullable: false),
+                    NumberChange = c.Int(nullable: false),
+                    Replacements = c.Int(nullable: false),
+                    Openings = c.Int(nullable: false),
+                    MedianAnnualWage = c.Int(nullable: false),
+                })
+                .PrimaryKey(t => t.Id);
+
             AlterColumn("dbo.OccupationalGroups", "OccupationalCode", c => c.String());
         }
         
